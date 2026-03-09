b5546fa9afc93131d540a026521ee67429ad720b
AbderrahmanNaciri22 <abderrhmanen2205@gmail.com>
2026-03-09 12:48:08 +0000

    add structure

---
+++ README.md
@@ -0,0 +1,14 @@
+Pour install tout depandances principales : 
+    npm install express mongoose bcrypt jsonwebtoken dotenv cors morgan
+    
+Validation.
+
+npm install joi
+
+Sécurité.
+
+npm install helmet express-rate-limit
+
+Dépendances développement.
+
+npm install --save-dev nodemon
---
+++ src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+// app.use("/transaction", require("./routes/transaction.routes"));
+
+
+
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("Atlas connected")).catch(err => console.log(err));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
+
